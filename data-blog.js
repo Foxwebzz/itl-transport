e071664047cb64a48f5e2813a8c88a5544c3b90a
DESKTOP-UG3PI5T\Novakovic <nikola2312017@gmail.com>
2021-12-25 13:33:06 +0100

    master

---
+++ data-blog.js
@@ -0,0 +1,58 @@
+let blogsData = [
+    {
+        title: "This is title 1",
+        textBlock: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere suscipit libero impedit ad dicta doloribus molestias numquam, ipsa enim sint! Rem consequatur blanditiis impedit dolorem eveniet doloremque cum ea delectus!",
+        imgUrl: "./img/boxes-commpresed.jpg",
+        imgAlt: "Nikola",
+        moreBlogText: "this is paragraph1"
+    },
+    {
+        title: "This is title 2",
+        textBlock: "This is text 2",
+        imgUrl: "./img/Homesection.-compresedjpg.jpg",
+        imgAlt: "",
+        moreBlogText: "this is paragraph 2"
+    },
+    {
+        title: "This is title 3",
+        textBlock: "This is text 3",
+        imgUrl: "./img/relovcation-van-compresed.jpg",
+        imgAlt: "",
+        moreBlogText: "this is paragraph 3"
+    },
+    {
+        title: "This is title 3",
+        textBlock: "This is text 3",
+        imgUrl: "./img/relovcation-van-compresed.jpg",
+        imgAlt: "",
+        moreBlogText: "this is paragraph 3"
+    },
+    {
+        title: "This is title 3",
+        textBlock: "This is text 3",
+        imgUrl: "./img/relovcation-van-compresed.jpg",
+        imgAlt: "",
+        moreBlogText: "this is paragraph 3"
+    },
+    {
+        title: "This is title 3",
+        textBlock: "This is text 3",
+        imgUrl: "./img/relovcation-van-compresed.jpg",
+        imgAlt: "",
+        moreBlogText: "this is paragraph 3"
+    },
+    {
+        title: "This is title 3",
+        textBlock: "This is text 3",
+        imgUrl: "./img/relovcation-van-compresed.jpg",
+        imgAlt: "",
+        moreBlogText: "this is paragraph 3"
+    },
+    {
+        title: "This is title 3",
+        textBlock: "This is text 3",
+        imgUrl: "./img/relovcation-van-compresed.jpg",
+        imgAlt: "",
+        moreBlogText: "this is paragraph 3"
+    }
+]
---
+++ script.js
@@ -0,0 +1,48 @@
+const navigation = document.querySelector('.navigation')
+const ulNav = document.querySelector('.ul-nav')
+const barBtn = document.querySelector('.bar-nav')
+const showBlockText = document.querySelectorAll('.show-block-text')
+const readMoreBtn = document.querySelectorAll('.read-more-btn')
+const closeBtn = document.querySelectorAll('.btn-close')
+
+
+barBtn.addEventListener('click', function() {
+    ulNav.classList.toggle('active')
+})
+
+window.onscroll = function() {
+    var top = window.scrollY
+
+    if(top >= 30) {
+        navigation.classList.add('nav-active')
+    } else {
+        navigation.classList.remove('nav-active')
+    }
+}
+
+for (let i = 0; i < readMoreBtn.length; i++) {
+    readMoreBtn[i].addEventListener('click', function() {
+        showBlockText[i].classList.add('show-block-active')
+    })
+}
+for (let i = 0; i < closeBtn.length; i++) {
+    closeBtn[i].addEventListener('click', function() {
+        showBlockText[i].classList.remove('show-block-active')
+    })
+}
+
+
+
+// faq-section
+const moreBtn = document.querySelectorAll('.more-btn')
+const answer = document.querySelectorAll('.answer')
+for (let i = 0; i < moreBtn.length; i++) {
+    moreBtn[i].addEventListener('click', function() {
+        answer[i].classList.toggle ('active-faq')
+        moreBtn[i].classList.toggle ('active-faq-btn')
+    })
+}
+// endfaq
+
+
+
